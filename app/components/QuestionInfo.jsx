18e0b22562227f4b94c45bdf2b41bf7e05369a4d
Khen Price <khen@humtap.com>
2016-05-24 15:43:53 -0400

    added yes no btns

--- app/components/QuestionInfo.jsx
+++ app/components/QuestionInfo.jsx
@@ -8,14 +8,21 @@ module.exports = React.createClass({
     },
     render:function(){
         var deleteLink;
+        var answerBtns;
         if (this.props.isAdmin) {
             deleteLink = (<span className="pull-right text-uppercase delete-button" onClick={this.deleteQuestion}>&times;</span>);
+        } else {
+            answerBtns = (<span> 
+                            <button type="button" className="pull-right btn btn-success">Yes</button>
+                            <button type="button" className="pull-right btn btn-danger">No</button>
+                          </span>);
         }
         return(
             <div className="panel panel-default question-info">
                 <div className="panel-heading">
                     {this.props.info.question_text}
                     {deleteLink}
+                    {answerBtns}
                 </div>
             </div>
         )
