b154d377a2d52dc6c937b5c1b2511897bb84c7b8
Khen Price <khen@humtap.com>
2016-05-24 14:16:12 -0400

    non debug mode

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use("/", adminQuestionController);
 app.use("/", questionController);
 
-DEBUG = true;
+DEBUG = false;
 
 // log to console
 app.use(morgan('dev'));
